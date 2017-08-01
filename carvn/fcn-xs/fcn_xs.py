72effeedd02b4d795ab2922965af8c45ab198497
guuker <guojia@gmail.com>
2017-08-01 10:24:26 +0800

    code refine

--- carvn/fcn-xs/fcn_xs.py
+++ carvn/fcn-xs/fcn_xs.py
@@ -15,26 +15,43 @@ logger.setLevel(logging.INFO)
 ctx = mx.gpu(5)
 
 def main():
-    #carvn_root = '/raid5data/dplearn/carvn'
     carvn_root = ''
     num_classes = 2
-    model_dir = "./model3"
-    cutoff = 800
-    fcnxs = symbol_fcnxs.get_fcn32s_symbol(numclass=num_classes, workspace_default=1536)
-    fcnxs_model_prefix = model_dir+"/FCN32s_VGG16"
+    cutoff = None if args.cutoff==0 else args.cutoff
+    epochs = [74,31,27,19]
+    model_prefixes = ['VGG_FC_ILSVRC_16_layers', args.model_dir+"/FCN32s_VGG16", args.model_dir+"/FCN16s_VGG16", args.model_dir+"/FCN8s_VGG16"]
     if args.model == "fcn16s":
-        fcnxs = symbol_fcnxs.get_fcn16s_symbol(numclass=num_classes, workspace_default=1536)
-        fcnxs_model_prefix = model_dir+"/FCN16s_VGG16"
+      fcnxs = symbol_fcnxs.get_fcn16s_symbol(numclass=num_classes, workspace_default=1536)
+      fcnxs_model_prefix = model_prefixes[2]
+      load_prefix = model_prefixes[1]
+      lr = 1e-5
+      run_epochs = epochs[2]
+      load_epoch = epochs[1]
     elif args.model == "fcn8s":
-        fcnxs = symbol_fcnxs.get_fcn8s_symbol(numclass=num_classes, workspace_default=1536)
-        fcnxs_model_prefix = model_dir+"/FCN8s_VGG16"
+      fcnxs = symbol_fcnxs.get_fcn8s_symbol(numclass=num_classes, workspace_default=1536)
+      fcnxs_model_prefix = model_prefixes[3]
+      load_prefix = model_prefixes[2]
+      lr = 1e-6
+      run_epochs = epochs[3]
+      load_epoch = epochs[2]
+    else:
+      fcnxs = symbol_fcnxs.get_fcn32s_symbol(numclass=num_classes, workspace_default=1536)
+      fcnxs_model_prefix = model_prefixes[1]
+      load_prefix = model_prefixes[0]
+      lr = 1e-4
+      run_epochs = epochs[1]
+      load_epoch = epochs[0]
     arg_names = fcnxs.list_arguments()
-    _, fcnxs_args, fcnxs_auxs = mx.model.load_checkpoint(args.prefix, args.epoch)
+    print('loading', load_prefix, load_epoch)
+    print('lr', lr)
+    print('model_prefix', fcnxs_model_prefix)
+    print('running epochs', run_epochs)
+    _, fcnxs_args, fcnxs_auxs = mx.model.load_checkpoint(load_prefix, load_epoch)
     if not args.retrain:
-        if args.init_type == "vgg16":
-            fcnxs_args, fcnxs_auxs = init_fcnxs.init_from_vgg16(ctx, fcnxs, fcnxs_args, fcnxs_auxs)
-        elif args.init_type == "fcnxs":
-            fcnxs_args, fcnxs_auxs = init_fcnxs.init_from_fcnxs(ctx, fcnxs, fcnxs_args, fcnxs_auxs)
+      if args.model == "fcn16s" or args.model == "fcn8s":
+        fcnxs_args, fcnxs_auxs = init_fcnxs.init_from_fcnxs(ctx, fcnxs, fcnxs_args, fcnxs_auxs)
+      else:
+        fcnxs_args, fcnxs_auxs = init_fcnxs.init_from_vgg16(ctx, fcnxs, fcnxs_args, fcnxs_auxs)
     train_dataiter = FileIter(
         root_dir             = carvn_root,
         flist_name           = "../data/train.lst",
@@ -51,10 +68,10 @@ def main():
         ctx                 = ctx,
         symbol              = fcnxs,
         begin_epoch         = 0,
-        num_epoch           = 50,
+        num_epoch           = run_epochs,
         arg_params          = fcnxs_args,
         aux_params          = fcnxs_auxs,
-        learning_rate       = 1e-4,
+        learning_rate       = lr,
         momentum            = 0.99,
         wd                  = 0.0005)
     _metric = DiceMetric()
@@ -69,12 +86,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Convert vgg16 model to vgg16fc model.')
     parser.add_argument('--model', default='fcnxs',
         help='The type of fcn-xs model, e.g. fcnxs, fcn16s, fcn8s.')
-    parser.add_argument('--prefix', default='VGG_FC_ILSVRC_16_layers',
-        help='The prefix(include path) of vgg16 model with mxnet format.')
-    parser.add_argument('--epoch', type=int, default=74,
-        help='The epoch number of vgg16 model.')
-    parser.add_argument('--init-type', default="vgg16",
-        help='the init type of fcn-xs model, e.g. vgg16, fcnxs')
+    parser.add_argument('--model-dir', default='./model',
+        help='directory to save model.')
+    parser.add_argument('--cutoff', type=int, default=800,
+        help='cutoff size.')
     parser.add_argument('--retrain', action='store_true', default=False,
         help='true means continue training.')
     args = parser.parse_args()
